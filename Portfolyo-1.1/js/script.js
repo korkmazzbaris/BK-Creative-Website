3017454347182800f2bb2983883f34ab4a23a3cf
Barış Korkmaz <158577842+korkmazzbaris@users.noreply.github.com>
2024-03-15 10:38:50 +0300

    Portfolyo V1.1

---
+++ Portfolyo-1.1/js/script.js
@@ -0,0 +1,23 @@
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach( button =>{
+    button.addEventListener('click',()=>{
+        const faq = button.nextElementSibling;
+        const icon = button.children[1];
+
+        faq.classList.toggle('show');
+        icon.classList.toggle('rotate');
+    })
+} )
+
+slider.addEventListener.toggle("auto");
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
+
+const slider = document.querySelector("slider");
